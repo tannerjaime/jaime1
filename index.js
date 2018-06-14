bbc65e85b740c3f884c3e6fb4642f6e80e4d1ba0
Nathan Wells <nwwells@gmail.com>
2018-06-13 21:00:48 -0400

    spec'd out a bare-bones http server that parses numbers

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log('Hello World!');
+
+const http = require('http');
+
+const requestListener = (req, res) => {
+  const maybeNumber = req.url.substr(1);
+  if (/^\d+$/.test(maybeNumber)) {
+    const number = parseInt(maybeNumber);
+    console.log('got a request:', number);
+    res.end();
+  } else {
+    console.error('got a bad request:', maybeNumber);
+    res.statusCode = 400;
+    res.end();
+  }
+};
+
+const debug = eventName => (...args) => console.log(eventName, args);
+
+const server = http.createServer();
+
+server.on('close', debug('close'));
+// server.on('connection', debug('connection'));
+server.on('error', debug('error'));
+server.on('listening', debug('listening'));
+server.on('checkContinue', debug('checkContinue'));
+server.on('checkExpectation', debug('checkExpectation'));
+server.on('clientError', debug('clientError'));
+server.on('connect', debug('connect'));
+server.on('request', requestListener);
+server.on('upgrade', debug('upgrade'));
+
+server.listen(8123);
